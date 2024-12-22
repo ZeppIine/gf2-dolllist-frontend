63ade9f3419d93f0d2ee534fefc74cc778e54aa1
ZeppIine <kim0904min@gmail.com>
2024-12-22 19:36:09 +0900

    인터페이스 변경

--- src/Container.js
+++ src/Container.js
@@ -1,39 +1,39 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-const Container = (props) => {
+const Container = (dolls) => {
   const [specsExpanded, setSpecsExpanded] = useState(false);
 
   const toggleSpecs = () => {
     setSpecsExpanded((prevState) => !prevState);
   };
-  console.log(props);
+  console.log(dolls);
 
   return (
     <>
       <div className="container">
         <img
           className="doll-img"
-          src={`https://i.namu.wiki/i/${props.dolls.img}.webp`}
-          alt={`${props.dolls.name} 이미지`}
+          src={`https://i.namu.wiki/i/${dolls.img}.webp`}
+          alt={`${dolls.name} 이미지`}
           onClick={toggleSpecs}
         />
         <a
-          href={`https://www.youtube.com/results?search_query=소녀전선2%20${props.dolls.name}`}
+          href={`https://www.youtube.com/results?search_query=소녀전선2%20${dolls.name}`}
           target="_blank"
           rel="noreferrer"
         >
           <div className="doll-name">
-            {props.dolls.name}
+            {dolls.name}
             {specsExpanded && (
               <pre onClick={toggleSpecs}>
-                {`${props.dolls.dclass}(${props.dolls.type})`}
+                {`${dolls.dclass}(${dolls.type})`}
               </pre>
             )}
           </div>
         </a>
         <div className="doll-star">
-          {[...Array(props.dolls.star)].map((_, index) => (
+          {[...Array(dolls.star)].map((_, index) => (
             <FaStar key={index} />
           ))}
         </div>
