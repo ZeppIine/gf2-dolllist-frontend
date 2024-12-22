0b38861b5d5ee5761ac835b7007bf48732899b99
ZeppIine <kim0904min@gmail.com>
2024-12-22 19:40:30 +0900

    인터페이스 변경경

--- src/Container.js
+++ src/Container.js
@@ -7,33 +7,33 @@ const Container = (dolls) => {
   const toggleSpecs = () => {
     setSpecsExpanded((prevState) => !prevState);
   };
-  console.log(dolls);
+  console.log(dolls.doll);
 
   return (
     <>
       <div className="container">
         <img
           className="doll-img"
-          src={`https://i.namu.wiki/i/${dolls.img}.webp`}
-          alt={`${dolls.name} 이미지`}
+          src={`https://i.namu.wiki/i/${dolls.doll.img}.webp`}
+          alt={`${dolls.doll.name} 이미지`}
           onClick={toggleSpecs}
         />
         <a
-          href={`https://www.youtube.com/results?search_query=소녀전선2%20${dolls.name}`}
+          href={`https://www.youtube.com/results?search_query=소녀전선2%20${dolls.doll.name}`}
           target="_blank"
           rel="noreferrer"
         >
           <div className="doll-name">
-            {dolls.name}
+            {dolls.doll.name}
             {specsExpanded && (
               <pre onClick={toggleSpecs}>
-                {`${dolls.dclass}(${dolls.type})`}
+                {`${dolls.doll.dclass}(${dolls.doll.type})`}
               </pre>
             )}
           </div>
         </a>
         <div className="doll-star">
-          {[...Array(dolls.star)].map((_, index) => (
+          {[...Array(dolls.doll.star)].map((_, index) => (
             <FaStar key={index} />
           ))}
         </div>
